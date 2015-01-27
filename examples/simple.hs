68e9fffafde4722d398d6465f806ffed034b5aa2
Jeffrey Rosenbluth <Jeffrey.Rosenbluth@gmail.com>
2015-01-27 12:59:44 -0500

    Update simple.hs

--- examples/simple.hs
+++ examples/simple.hs
@@ -9,9 +9,9 @@ svg content = do
 
 contents :: Svg ()
 contents = do
-  rect_ [width_ "100%", height_ "100%", fill_ "red"]
+  rect_   [width_ "100%", height_ "100%", fill_ "red"]
   circle_ [cx_ "150", cy_ "100", r_ "80", fill_ "green"]
-  text_ [x_ "150", y_ "125", fontSize_ "60", textAnchor_ "middle", fill_ "white"] "SVG"
+  text_   [x_ "150", y_ "125", font_size_ "60", text_anchor_ "middle", fill_ "white"] "SVG"
 
 
 main :: IO ()
