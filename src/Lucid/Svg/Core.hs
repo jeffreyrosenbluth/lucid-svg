b2a23ebb071a67178f8108995ce9b1611883be23
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2016-01-31 11:46:13 -0500

    more comments

--- src/Lucid/Svg/Core.hs
+++ src/Lucid/Svg/Core.hs
@@ -24,6 +24,7 @@ module Lucid.Svg.Core
 , makeXmlElementNoEnd
 , element
 , with
+, nil
   -- * Rendering
 , renderBS
 , renderToFile
@@ -31,12 +32,8 @@ module Lucid.Svg.Core
 ) where
 
 import           Blaze.ByteString.Builder (Builder)
-import qualified Blaze.ByteString.Builder as Blaze
-import           Blaze.ByteString.Builder.Html.Utf8 ( fromText
-                                                    , fromHtmlEscapedText
-                                                    , fromHtmlEscapedString
-                                                    , fromHtmlEscapedLazyText )
-import qualified Blaze.ByteString.Builder.Html.Utf8 as Blaze
+import qualified Blaze.ByteString.Builder as BB
+import qualified Blaze.ByteString.Builder.Html.Utf8 as BB
 import qualified Data.ByteString.Lazy as LB
 import           Data.ByteString.Lazy (ByteString)
 import           Data.Maybe (fromMaybe)
@@ -56,22 +53,24 @@ import qualified Data.Text.Lazy.Encoding as LT
 data Attribute = Attribute !Text !Text
   deriving (Show,Eq)
 
+-- | Type of an SVG element.
 type Element = HashMap Text Text -> Builder
 
 instance IsString Element where
   fromString = toElement
 
+-- | Things that can be converted to SVG elements.
 class ToElement a where
   toElement :: a -> Element
 
 instance ToElement String where
-  toElement = const . fromHtmlEscapedString
+  toElement = const . BB.fromHtmlEscapedString
 
 instance ToElement Text where
-  toElement = const . fromHtmlEscapedText
+  toElement = const . BB.fromHtmlEscapedText
 
 instance ToElement LT.Text where
-  toElement = const . fromHtmlEscapedLazyText
+  toElement = const . BB.fromHtmlEscapedLazyText
 
 --------------------------------------------------------------------------------
 -- Combinators
@@ -92,27 +91,32 @@ with ml attrs attrs' = ml (unionAttrs (M.fromListWith (<>) (map toPair attrs)) a
   where
     toPair (Attribute x y) = (x,y)
 
+-- | Used to make specific SVG element builders.
 element :: Text -> [Attribute] -> Element -> Element
 element name attrs ml = with (makeElement name ml) attrs
 
+-- | The empty element.
 nil :: Element
 nil = mempty
 
+-- | Make an SVG element builder
 makeElement :: Text -> Element -> Element
 makeElement name children attrs =
-     s2b "<" <> fromText name
+     s2b "<" <> BB.fromText name
   <> foldlMapWithKey buildAttr attrs <> s2b ">"
   <> children mempty
-  <> s2b "</" <> fromText name <> s2b ">"
+  <> s2b "</" <> BB.fromText name <> s2b ">"
 
+-- | Make an SVG element builder with no end tag.
 makeElementNoEnd :: Text -> Element
 makeElementNoEnd name attrs =
-     s2b "<" <> fromText name
+     s2b "<" <> BB.fromText name
   <> foldlMapWithKey buildAttr attrs <> s2b ">"
 
+-- | Make an XML element with no end tag.
 makeXmlElementNoEnd :: Text -> Element
 makeXmlElementNoEnd name attrs =
-     s2b "<" <> fromText name
+     s2b "<" <> BB.fromText name
   <> foldlMapWithKey buildAttr attrs <> s2b "/>"
 
 -- | Folding and monoidally appending attributes.
@@ -120,23 +124,23 @@ foldlMapWithKey :: Monoid m => (k -> v -> m) -> HashMap k v -> m
 foldlMapWithKey f = M.foldlWithKey' (\m k v -> m <> f k v) mempty
 
 s2b :: String -> Builder
-s2b = Blaze.fromString
+s2b = BB.fromString
 
 -- | Build and encode an attribute.
 buildAttr :: Text -> Text -> Builder
 buildAttr key val =
   s2b " " <>
-  fromText key <>
+  BB.fromText key <>
   if val == mempty
     then mempty
-    else s2b "=\"" <> fromHtmlEscapedText val <> s2b "\""
+    else s2b "=\"" <> BB.fromHtmlEscapedText val <> s2b "\""
 
 --------------------------------------------------------------------------------
 -- Rendering
 
 -- | Render a 'Element' to lazy bytestring.
 renderBS :: Element -> ByteString
-renderBS ml = Blaze.toLazyByteString $ ml mempty
+renderBS ml = BB.toLazyByteString $ ml mempty
 
 -- | Render a 'Element' to a file.
 renderToFile :: FilePath -> Element -> IO ()
